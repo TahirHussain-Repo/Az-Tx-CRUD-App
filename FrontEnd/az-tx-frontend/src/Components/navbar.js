daf0dc3525f031a39eeda33f3719bfd646477006
Tahir Hussain <tahir@waveaxis.com>
2023-11-02 23:47:20 -0500

    authentication added and tested

--- FrontEnd/az-tx-frontend/src/Components/navbar.js
+++ FrontEnd/az-tx-frontend/src/Components/navbar.js
@@ -5,24 +5,27 @@ import {
     Route,
     NavLink,
 } from "react-router-dom";
+import { useAuth0 } from '@auth0/auth0-react';
+import Button from 'react-bootstrap/Button';
+
 import Landing from "./landing";
 import SurveyForm from "./survey";
 import Results from "./results"
-import Login from "./login";
 import Signup from "./signup";
 
 const Navbar = () => {
 
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
   
-    return (
-        <Router>
-          <div>
-            <nav className="navbar navbar-expand bg-white py-4 shadow">
-              <div className="container">
-                <NavLink exact to="/" className="btn btn-outline-primary mr-4">
-                  Az-Tx-Health
-                </NavLink>
-    
+  return (
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand bg-white py-4 shadow">
+            <div className="container">
+              <NavLink exact to="/" className="btn btn-outline-info mr-4">
+                Az-Tx-Health
+              </NavLink>
+              {isAuthenticated &&(
                 <ul className="navbar-nav mr-auto">
                   <li className="nav-item">
                     <NavLink to="/survey" className="nav-link">
@@ -35,40 +38,45 @@ const Navbar = () => {
                     </NavLink>
                   </li>
                 </ul>
-                <AuthButtons />
-              </div>
-            </nav>
-            <div>
-              <Routes>
-                <Route path="/results" element={<Results />} />
-                <Route path="/survey" element={<SurveyForm />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/" element={<Landing />} />
-              </Routes>
+              )}
+              {!isAuthenticated && (
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item" style={{paddingRight: '15px'}}>
+                    <Button onClick={() => loginWithRedirect()} variant="outline-info">
+                      Login
+                    </Button>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/signup" className="btn btn-outline-info mr-4">
+                      Signup
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+              {isAuthenticated && (
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item" style={{paddingRight: '15px'}}>
+                    <Button onClick={() => logout()} variant="outline-info">
+                      Logout
+                    </Button>
+                  </li>
+                </ul>
+              )}
             </div>
+          </nav>
+          <div>
+            <Routes>
+              <Route path="/results" element={<Results />} />
+              <Route path="/survey" element={<SurveyForm />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/" element={<Landing />} />
+            </Routes>
           </div>
-        </Router>
-    );
+        </div>
+      </Router>
+  );
 }
 
 export default Navbar
-
-function AuthButtons() {
-    return (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <NavLink to="/login" className="nav-link">
-            Login
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/signup" className="nav-link">
-            Signup
-          </NavLink>
-        </li>
-      </ul>
-    );
-  }
 
   
--- FrontEnd/az-tx-frontend/src/index.js
+++ FrontEnd/az-tx-frontend/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import {Auth0Provider} from '@auth0/auth0-react';
+
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
