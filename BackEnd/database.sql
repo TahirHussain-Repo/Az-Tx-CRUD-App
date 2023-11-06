1035e1c7baf1359bb6a65bcba950bbbdc6f25f0d
Tahir Hussain <tahir@waveaxis.com>
2023-11-06 17:21:01 -0600

    database sql changes

--- BackEnd/database.sql
+++ BackEnd/database.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE survey_db;
 
+CREATE TABLE survey_users (
+    id SERIAL PRIMARY KEY,
+    auth0_id VARCHAR(255) UNIQUE NOT NULL, -- This will store the Auth0 user ID
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
 CREATE TABLE survey_questions (
     id SERIAL PRIMARY KEY,
     question_text TEXT NOT NULL
@@ -18,3 +24,6 @@ CREATE TABLE user_responses (
     question_id INT REFERENCES survey_questions(id) ON DELETE CASCADE,
     answer_id INT REFERENCES survey_answers(id)
 );
+
+ALTER TABLE user_responses ADD COLUMN user_id INT REFERENCES survey_users(id);
+
